d2615a77def063e955ed7fe987c15c02dddf3c30
sSAR <138690654+zUltimaPG3D@users.noreply.github.com>
2026-01-31 11:39:58 +0100

    chore: remove a bunch of useless stuff in imgui

--- app/src/main/jni/frontend.h
+++ app/src/main/jni/frontend.h
@@ -3,12 +3,9 @@
 #define MENU
 
 namespace Menu {
-
 	ImVec4 color = ImVec4(1, 1, 1, 1);
 
 	void DrawMenu() {
-		static bool show_demo_window = false;
-		static bool show_another_window = false;
 		static ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
 
 		ImGuiIO& io = ImGui::GetIO();
@@ -17,42 +14,21 @@ namespace Menu {
 		std::string text = "FPS: " + std::to_string((int)io.Framerate);
 		draw_list->AddText(ImVec2(6, 6), ImColor(1.0f, 1.0f, 1.0f, 1.0f), text.c_str());
 
-		if (show_demo_window)
-			ImGui::ShowDemoWindow(&show_demo_window);
-
+		ImGui::Begin("Town Restoration");
 		{
 			static float f = 0.0f;
 			static int counter = 0;
 
-			ImGui::Begin("Town Restoration");
 
 			// ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
-			
-			if (ImGui::CollapsingHeader("Information and Links", ImGuiTreeNodeFlags_DefaultOpen)) 
+
+			if (ImGui::CollapsingHeader("Information and Links", ImGuiTreeNodeFlags_DefaultOpen))
 			{
 				if (ImGui::Button("GitHub Repo")) OpenURL("https://github.com/zUltimaPG3D/town-restoration/");
 				if (ImGui::Button("Community Discord")) OpenURL("https://discord.gg/keB3RNumVt");
                 ImGui::Text("* town-restoration by sSAR");
                 ImGui::Text("* toro-webserver by sSAR");
                 ImGui::Text("  - partially based on puzzle by synzr");
-			}
-
-			ImGui::Checkbox("Software Cursor Rendering", &io.MouseDrawCursor);
-
-			if (ImGui::CollapsingHeader("Misc. features"))
-			{
-                ImGui::BeginDisabled(SaveData == nullptr);
-                if (ImGui::Button("Clear Data")) {
-                    old_SaveDataClear(SaveData);
-                }
-                ImGui::EndDisabled();
-				if (ImGui::Checkbox("Unlock FPS", &uncap_fps))
-                {
-                    if (uncap_fps) {
-                        set_vSyncCount(0);
-                        set_targetFrameRate(144);
-                    }
-                }
 			}
 
 			ImGui::End();
